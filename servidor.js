4931fc4b6dd56962f2a6efb21bbf8825e07ca44f
alu0100894325 <alu0100894325@ull.edu.es>
2017-03-05 15:15:58 +0000

    Modificado puerto para Heroku

--- servidor.js
+++ servidor.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
     res.render('index', { title: 'Index' });
 });
 
-var server = app.listen(8080, function(){
+var server = app.listen(process.env.PORT || 8080, function(){
         var host = server.address().address;
         var port = server.address().port;
         
